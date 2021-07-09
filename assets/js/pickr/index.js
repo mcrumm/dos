f801f2c1a5c8b9163de1fe736f72d3abb0d478b0
Michael Crumm <mike@crumm.net>
2021-07-09 13:16:18 -0700

    Add data-pickr-config

--- assets/js/pickr/index.js
+++ assets/js/pickr/index.js
@@ -1,13 +1,33 @@
 import flatpickr from "flatpickr";
 
 const Pickr = {
+  buildConfig() {
+    // Build the initial config map from the
+    // `data-pickr-config` DOM attribute
+    let config = (this.el.dataset.pickrConfig &&
+      JSON.parse(this.el.dataset.pickrConfig)) || {}
+
+    // Combine the config with a default dateFormat.
+    // This also ensures the required `wrap: true`
+    // option cannot be overwritten.
+    return { dateFormat: "Y-m-d", ...config, wrap: true }
+  },
   mounted() {
-    this.pickr = flatpickr(this.el, {
-      wrap: true,
-      altInput: this.el.dataset.pickrAltFormat ? true : false,
-      altFormat: this.el.dataset.pickrAltFormat || "d M Y",
-      dateFormat: this.el.dataset.pickrDateFormat || "Y-m-d"
-    })
+    const config = this.buildConfig()
+    this.pickr = flatpickr(this.el, config)
+    this.config = config
+  },
+  updated() {
+    let config = this.buildConfig()
+    let wasConfig = this.config
+    if (config !== wasConfig) {
+      this.pickr.destroy()
+      this.pickr = flatpickr(this.el, config)
+      this.config = config
+    }
+  },
+  destroyed() {
+    this.pickr.destroy()
   }
 }
 
--- lib/dos_web/live/pickr_live.ex
+++ lib/dos_web/live/pickr_live.ex
@@ -8,12 +8,12 @@ defmodule DosWeb.PickrLive do
 
     <form id="pickr-form" action="#" phx-change="validate">
       <label for="starts-at">Starts at</label>
-      <div id="starts-at-pickr" class="flatpickr" phx-update="ignore" phx-hook="Pickr">
+      <div id="starts-at-pickr" class="flatpickr" phx-update="ignore" phx-hook="Pickr" data-pickr-config="<%= @config %>">
         <input type="text" id="starts-at" name="pickr[starts_at]" placeholder="Select Start Date.." data-input />
       </div>
 
       <label for="ends-at">Ends at</label>
-      <div id="ends-at-pickr" class="flatpickr" phx-update="ignore" phx-hook="Pickr" data-pickr-alt-format="Y-m-d">
+      <div id="ends-at-pickr" class="flatpickr" phx-update="ignore" phx-hook="Pickr" data-pickr-config="<%= @config %>">
         <input type="text" id="ends-at" name="pickr[ends_at]" placeholder="Select End Date.." data-input />
       </div>
     </form>
@@ -21,16 +21,45 @@ defmodule DosWeb.PickrLive do
     <%= if @params do %>
     <p>Values: <code><%= @params %></code></p>
     <% end %>
+
+    <form id="format" action="#" phx-change="format">
+    <label>Alt Format</label>
+    <input type="text" name="format" value="<%= @format %>"/>
+    </form>
     """
   end
 
   @impl Phoenix.LiveView
   def handle_event("validate", %{"pickr" => _} = params, socket) do
-    {:noreply, assign(socket, :params, inspect(params))}
+    {:noreply, assign(socket, :params, inspect(params, pretty: true))}
+  end
+
+  def handle_event("format", %{"format" => ""}, socket) do
+    socket =
+      socket
+      |> assign(:format, "")
+      |> assign(:config, Jason.encode!(%{"altInput" => false}))
+
+    {:noreply, socket}
+  end
+
+  def handle_event("format", %{"format" => format}, socket) do
+    socket =
+      socket
+      |> assign(:format, format)
+      |> assign(:config, Jason.encode!(%{"altInput" => true, "altFormat" => format}))
+
+    {:noreply, socket}
   end
 
   @impl Phoenix.LiveView
   def mount(_, _, socket) do
-    {:ok, assign(socket, :params, nil)}
+    socket =
+      socket
+      |> assign(:params, nil)
+      |> assign(:format, "")
+      |> assign(:config, Jason.encode!(%{}))
+
+    {:ok, socket}
   end
 end
